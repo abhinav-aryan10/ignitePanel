39e89fe6619bc652f05a7993453f993dc77e69c1
Abhinav Aryan <Abhinav.Aryan@Triconinfotech.com>
2023-08-28 12:01:41 +0530

    feat:test env setup done

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,16 @@
+/**
+ * @jest-environment jsdom
+ */
+
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// test('renders learn react link', () => {
+//   render(<App />);
+// });
+
+test('use jsdom in this test file', () => {
+  const element = document.createElement('div');
+  expect(element).not.toBeNull();
 });
